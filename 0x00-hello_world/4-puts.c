c9fabc06c712ef24ff2ea5a40059c110611afedd
M0habAli <mohabali1997@googlemail.com>
2023-10-06 20:31:34 -0700

    Mohab

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- *Main bla bla bla
- * asokg  hkj h hdk j
- *  hsj hkjh kj hkjg h
- *   hhkj
- *    k kdg ks h
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
